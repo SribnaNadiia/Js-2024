586506abec4ca1424c7976e67f25b10c2c294d5f
Sribna30 <bellasark9669@icloud.com>
2024-02-22 22:40:21 +0100

    add to Home Work

---
+++ HomeWork/HomeWork1.js
@@ -0,0 +1,86 @@
+/*--------------1.1------------- */
+
+let x1 = 'hello';
+console.log(x1);
+
+let x2 = 'owu';
+console.log(x2);
+
+let x3 = 'com';
+console.log(x3);
+
+let x4 = 'ua';
+console.log(x4);
+
+let x5 = '1';
+console.log(x5);
+
+/*-----------------------------*/
+
+let a = 1;
+console.log(a);
+
+let b = 10;
+console.log(b);
+
+let c = -999;
+console.log(c);
+
+let d = 123;
+console.log(d);
+
+let e = 3.14;
+console.log(e);
+
+let f = 2.7;
+console.log(f);
+
+let g = 16;
+console.log(g);
+
+/*-----------------------------*/
+
+let y1 = true;
+console.log(y1);
+
+let y2 = false;
+console.log(y2);
+
+/*--------------1.2------------- */
+
+ let firstName = 'Sribna';
+ let middleName = 'Nadiia';
+ let lastName = 'Sergiivna';
+ let space = ' ';
+
+ let sen = firstName + space + middleName + space + lastName;
+ console.log(sen)
+
+/*--------------1.3------------- */
+
+let aaa = 100;
+console.log(typeof aaa, aaa);
+
+let bbb = '100';
+console.log(bbb);
+
+let ccc = true;
+console.log(true);
+
+/*--------------1.4------------- */
+
+let name = prompt('Nadiia');
+console.log(name);
+
+let lastname = prompt('Sergiivna');
+console.log(lastname);
+
+let age = prompt('22');
+console.log(age);
+
+
+
+
+
+
+
